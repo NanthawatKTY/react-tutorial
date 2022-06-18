ad35005ed06e4b58b1c37414fd8e7e9224941ee5
[NanthawtKTY] <[nanthawat.tae123@gmail.com]>
2022-06-18 14:40:20 +0700

    Added CSS Module

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,8 +1,10 @@
-import './Box.css';
+import styles from './Box.module.css';
 
 function Box(props) {
     return(
-       <div className='box'>
+       <div className={styles.box}>
+           <h3 className={styles.title}>{props.title}</h3>
+
            {/* children = data that was passed from html tag in AppLayoutComponent */}
            {props.children}
        </div> 
--- src/views/AppLayoutComponent.js
+++ src/views/AppLayoutComponent.js
@@ -1,23 +1,23 @@
 import './AppLayoutComponent.css';
 import Box from '../components/Box';
 import Grid from '../components/Grid';
+import Notification from '../components/notification/Notification';
 
 function AppLayoutComponent() {
     return (
         <section className='app-section'>
             <div className="app-container">
                 <h2>Farm Dashboard</h2>
+                <Notification title="Hi" description="We are here for peace."/>
                 <Grid columns={3}>
-                    <Box>
-                        <h3>ออเดอร์ต่อวัน</h3>
+                    <Box title="Order per day">
                         <ul>
                             <li>3 กันยายน - 1,582 ออเดอร์</li>
                             <li>2 กันยายน - 1,649 ออเดอร์</li>
                             <li>1 กันยายน - 1,755 ออเดอร์</li>
                         </ul>
                     </Box>
-                    <Box>
-                        <h3>ยอดขายสินค้ายอดนิยม</h3>
+                    <Box title="ยอดขายสินค้ายอดนิยม">
                         <p>หมายเหตุ : เป็นราคาโดยประมาณเท่านั้น</p>
                         <ol>
                             <li>แครอท ~ 200,000 บาท</li>
@@ -25,8 +25,7 @@ function AppLayoutComponent() {
                             <li>แตงกวา ~ 105,000 บาท</li>
                         </ol>
                     </Box>
-                    <Box>
-                        <h4>Quote of the day</h4>
+                    <Box title="Quote of the day">
                         <p>Be the best of yourself.</p>
                         <p>by me</p>
                     </Box>
